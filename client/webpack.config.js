33e6e0d3f826011e3f6b501fd8083be92733bf2a
maiaferrer <mferrer2@uncc.edu>
2023-03-04 14:01:09 -0500

    added icons in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -40,6 +40,13 @@ module.exports = () => {
         start_url: '/',
         publicPath: '/',
         // TODO: Add icons
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+            destination: path.join("assets", "icons"),
+          },
+        ],
       }),
     ],
 
